f5aa7b1d6d064d799d837e167f682eb8b94981bf
Dsolm <dsola2405@gmail.com>
2024-11-28 19:52:31 +0100

    Made texture and model loading faster

--- src/texture.rs
+++ src/texture.rs
@@ -1,5 +1,8 @@
-use image::GenericImageView;
+use std::time::Instant;
+
+// use image::GenericImageView;
 use anyhow::*;
+use stb_image::{image::load_from_memory, stb_image::{stbi_image_free, stbi_load_from_memory}};
 
 pub struct Texture {
     #[allow(unused)]
@@ -15,22 +18,40 @@ impl Texture {
         bytes: &[u8], 
         label: &str
     ) -> Result<Self> {
-        let img = image::load_from_memory(bytes)?;
-        Self::from_image(device, queue, &img, Some(label))
+
+        unsafe {
+            let begin = Instant::now();
+
+            let mut width: i32 = 0;
+            let mut height: i32 = 0;
+            let mut real_channels: i32 = 0;
+            let actual_channels = 4;
+            let buffer = stbi_load_from_memory(bytes.as_ptr() as _ , bytes.len() as i32, &mut width, &mut height, &mut real_channels, actual_channels);
+
+            if buffer.is_null() {
+                log::warn!("Invalid texture with label {}", label);
+                bail!("Image not found"); // TODO: Implement proper error handling.
+            }
+
+            let data = std::slice::from_raw_parts(buffer, (width*height*actual_channels) as usize);
+            println!("stb_image::image::load_from_memory for label: {} took {}ms", label, begin.elapsed().as_millis());
+            let res = Self::from_image(device, queue, data, width as u32, height as u32, Some(label));
+            stbi_image_free(buffer as _);
+            res
+        }
     }
 
     pub fn from_image(
         device: &wgpu::Device,
         queue: &wgpu::Queue,
-        img: &image::DynamicImage,
+        data: &[u8],
+        width: u32,
+        height: u32,
         label: Option<&str>
     ) -> Result<Self> {
-        let rgba = img.to_rgba8();
-        let dimensions = img.dimensions();
-
         let size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
+            width: width,
+            height: height,
             depth_or_array_layers: 1,
         };
         let texture = device.create_texture(
@@ -40,7 +61,7 @@ impl Texture {
                 mip_level_count: 1,
                 sample_count: 1,
                 dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                format: wgpu::TextureFormat::Rgba8Unorm,
                 usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
                 view_formats: &[],
             }
@@ -53,11 +74,11 @@ impl Texture {
                 mip_level: 0,
                 origin: wgpu::Origin3d::ZERO,
             },
-            &rgba,
+           data,
             wgpu::ImageDataLayout {
                 offset: 0,
-                bytes_per_row: Some(4 * dimensions.0),
-                rows_per_image: Some(dimensions.1),
+                bytes_per_row: Some(4 * width),
+                rows_per_image: Some(height),
             },
             size,
         );
@@ -76,5 +97,45 @@ impl Texture {
         );
         
         Ok(Self { texture, view, sampler })
+    }
+
+    pub const DEPTH_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth32Float; // 1.
+    
+    pub fn create_depth_texture(device: &wgpu::Device, config: &wgpu::SurfaceConfiguration, label: &str) -> Self {
+        let size = wgpu::Extent3d { // 2.
+            width: config.width.max(1),
+            height: config.height.max(1),
+            depth_or_array_layers: 1,
+        };
+        let desc = wgpu::TextureDescriptor {
+            label: Some(label),
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: Self::DEPTH_FORMAT,
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT // 3.
+                | wgpu::TextureUsages::TEXTURE_BINDING,
+            view_formats: &[],
+        };
+        let texture = device.create_texture(&desc);
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = device.create_sampler(
+            &wgpu::SamplerDescriptor { // 4.
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Linear,
+                min_filter: wgpu::FilterMode::Linear,
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                compare: Some(wgpu::CompareFunction::LessEqual), // 5.
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 100.0,
+                ..Default::default()
+            }
+        );
+
+        Self { texture, view, sampler }
     }
 }
